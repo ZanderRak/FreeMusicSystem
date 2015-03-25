6438bf3e2dc533b7c1dc4ddd35a787377ab373cb
Serg Shapoval <acidserg161@gmail.com>
2015-03-26 00:41:32 +0200

    Wrote sysout about ostap

--- FMS/src/main/java/com/rakshap/fms/TestRep.java
+++ FMS/src/main/java/com/rakshap/fms/TestRep.java
@@ -3,7 +3,7 @@ package com.rakshap.fms;
 public class TestRep {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("ostap likes to suck dicks!");
 
 	}
 
